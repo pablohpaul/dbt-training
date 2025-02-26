fbcb2fb49b5b2dd884a00ee0e591b3c23eca4114
Pablo H. Paul <pablo.paul@blend360.com>
2025-02-26 18:43:57 +0000

    developing stage folders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,17 @@
+SELECT o.orderid, 
+        o.orderdate, 
+        o.shipdate, 
+        o.shipmode,
+        o.ordersellingprice - o.ordercostprice as orderprofit,
+        o.ordersellingprice,
+        o.ordercostprice,
+        c.customername, 
+        c.country,
+        p.category, 
+        p.productname, 
+        p.subcategory
+FROM {{ ref('raw_order') }} as o
+LEFT JOIN {{ ref('raw_customer') }} as c
+ON o.customerid = c.customerid
+LEFT JOIN {{ ref('raw_product') }} as p
+ON o.productid = p.productid
